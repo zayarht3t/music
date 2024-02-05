2db6de4cb66584c84e3665640ca6685781ef6608
zayarht3t <zayy4699@gmail.com>
2024-02-05 22:13:28 +0630

    first commit

---
+++ src/components/Pages/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './css/About.scss';
+import Container from "../fragment/Container";
+
+const About = () => {
+    return (
+        <Container>
+            <div className={"About"}>
+                {/*<div className="Dark-mode">
+                    <h5>Dark mode to be available soon</h5>
+                    <IconButton>
+                        <Brightness3/>
+                    </IconButton>
+                </div>*/}
+            </div>
+        </Container>
+    );
+}
+
+export default About;
---
+++ src/components/fragment/Brand.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "../assets/scss/Brand.scss";
+import Logo from "../assets/img/headphonesLogo.svg"
+
+class Brand extends React.Component {
+    render() {
+        return (
+            <div  className={"brand"}>
+                <Link to={"/home"}>
+                    <h1>
+                        For You
+                    </h1>
+                </Link>
+            </div>
+        );
+    }
+}
+
+export default Brand;
